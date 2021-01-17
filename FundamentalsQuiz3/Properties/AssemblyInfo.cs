441357fa4528fd0532694d2970ad0081743f1dbd
joemacko <joemacko58@gmail.com>
2021-01-17 13:16:56 -0500

    Added DevTeamsProject to GitHub

---
+++ FundamentalsQuiz3/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FundamentalsQuiz3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FundamentalsQuiz3")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a7f84bfa-aead-4550-b7ca-af5892a12cd1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FundamentalsQuiz3/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FundamentalsQuiz3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public string Question7(string anotherName)
+        {
+            return ($"Hello, {anotherName}");
+        }
+    }
+
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public double Question10 (DateTime birthday)
+        {
+            Console.WriteLine($"You are {DateTime);
+        }
+    }
+}
